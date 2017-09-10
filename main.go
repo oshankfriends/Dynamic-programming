71f800ee04e5799d003d85cd07b93cbb21dcf9a3
oshank <oshankfriends@gmail.com>
2017-09-10 19:41:17 +0530

    added main.go file

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func minSteps(step []int, sum int) int {
+	if sum == 0 {
+		return 0
+	}
+	res := math.MaxInt32
+	for _, val := range step {
+		if val <= sum {
+			sub_res := minSteps(step, sum-val)
+			if sub_res != math.MaxInt32 && sub_res+1 < res {
+				res = sub_res + 1
+			}
+		}
+
+	}
+	return res
+}
+func main() {
+	steps := []int{1, 2, 3, 4, 5}
+	fmt.Println(minSteps(steps, 18))
+}
+
